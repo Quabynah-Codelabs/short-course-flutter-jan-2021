3cfbbd71495f61c724f227112c79efbcb51d5891
Dennis Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2021-03-05 16:40:32 +0000

    Update theme.dart

--- taufiq/blog_app_project/lib/shared/src/theme.dart
+++ taufiq/blog_app_project/lib/shared/src/theme.dart
@@ -2,37 +2,41 @@ import 'package:blog_app_project/shared/shared.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+final _kDefaultTextStyle = GoogleFonts.poppins();
+
 final kDefaultTextTheme = TextTheme(
-  headline1: GoogleFonts.poppins(
+  headline1: _kDefaultTextStyle.copyWith(
       fontSize: 98, fontWeight: FontWeight.w300, letterSpacing: -1.5),
-  headline2: GoogleFonts.poppins(
+  headline2: _kDefaultTextStyle.copyWith(
       fontSize: 61, fontWeight: FontWeight.w300, letterSpacing: -0.5),
-  headline3: GoogleFonts.poppins(fontSize: 49, fontWeight: FontWeight.w500),
-  headline4: GoogleFonts.poppins(
+  headline3:
+      _kDefaultTextStyle.copyWith(fontSize: 49, fontWeight: FontWeight.w500),
+  headline4: _kDefaultTextStyle.copyWith(
     fontSize: 35,
     fontWeight: FontWeight.w700,
     letterSpacing: 0.25,
     color: kBlackColor.withOpacity(kEmphasisHigh),
   ),
-  headline5: GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.w700),
-  headline6: GoogleFonts.poppins(
+  headline5:
+      _kDefaultTextStyle.copyWith(fontSize: 24, fontWeight: FontWeight.w700),
+  headline6: _kDefaultTextStyle.copyWith(
       fontSize: 18, fontWeight: FontWeight.w500, letterSpacing: 0.15),
-  subtitle1: GoogleFonts.poppins(
+  subtitle1: _kDefaultTextStyle.copyWith(
       fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
-  subtitle2: GoogleFonts.poppins(
+  subtitle2: _kDefaultTextStyle.copyWith(
       fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
-  bodyText1: GoogleFonts.poppins(
+  bodyText1: _kDefaultTextStyle.copyWith(
     fontSize: 16,
     fontWeight: FontWeight.w500,
     letterSpacing: 0.5,
     color: kBlackColor.withOpacity(kEmphasisLow),
   ),
-  bodyText2: GoogleFonts.poppins(
+  bodyText2: _kDefaultTextStyle.copyWith(
       fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-  button: GoogleFonts.poppins(
+  button: _kDefaultTextStyle.copyWith(
       fontSize: 14, fontWeight: FontWeight.w600, letterSpacing: 1.25),
-  caption: GoogleFonts.poppins(
+  caption: _kDefaultTextStyle.copyWith(
       fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
-  overline: GoogleFonts.poppins(
+  overline: _kDefaultTextStyle.copyWith(
       fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
 );
